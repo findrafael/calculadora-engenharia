77583714543dde5eb2a663cb4c0e39741b8f2bea
kayque791 <kayque32045@gmail.com>
2022-10-13 19:39:35 -0300

    :bug: BUG - correção de bug das variaveis

--- calc.py
+++ calc.py
@@ -1,7 +1,7 @@
 print('-------------------------')
 print('Olá, seja bem vindo à nossa calculadora!')
 
-print('Escolha uma opção para continuar:')
+print('Escolha uma operação para continuar:')
 print('1- Adição')
 print('2- Subtração')
 print('3- Multiplicação')
@@ -11,29 +11,29 @@ escolha = input('Digite a opção desejada:')
 
 match escolha:
     case "1": #ADIÇÃO
-        num1 = input("Digite o primeiro número: ")
-        num2 = input("Digite o segundo número: ")
+        num1 = int(input("Digite o primeiro número: "))
+        num2 = int(input("Digite o segundo número: "))
         resultado = num1 + num2
         print('A sua soma é: ', resultado)
         print('')
 
     case "2": #SUBTRAÇÃO
-        num1 = input("Digite o primeiro número: ")
-        num2 = input("Digite o segundo número: ")
+        num1 = int(input("Digite o primeiro número: "))
+        num2 = int(input("Digite o segundo número: "))
         resultado = num1 - num2
         print('A sua soma é: ', resultado)
         print('')
     
     case "3": #MULTIPLICAÇÃO
-        num1 = input("Digite o primeiro número: ")
-        num2 = input("Digite o segundo número: ")
+        num1 = int(input("Digite o primeiro número: "))
+        num2 = int(input("Digite o segundo número: "))
         resultado = num1 * num2
         print('A sua soma é: ', resultado)
         print('')
     
     case "4": #DIVISÃO
-        num1 = input("Digite o primeiro número: ")
-        num2 = input("Digite o segundo número: ")
+        num1 = int(input("Digite o primeiro número: "))
+        num2 = int(input("Digite o segundo número: "))
         resultado = num1 / num2
         print('A sua soma é: ', resultado)
         print('')
