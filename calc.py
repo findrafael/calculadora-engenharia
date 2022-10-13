ae6f5f0779a449a2603536b338e16c7cf7788a6b
Rafael Tavares <newsadnessboy@gmail.com>
2022-10-13 19:33:59 -0300

    adição código calculadora

---
+++ calc.py
@@ -0,0 +1,39 @@
+print('-------------------------')
+print('Olá, seja bem vindo à nossa calculadora!')
+
+print('Escolha uma opção para continuar:')
+print('1- Adição')
+print('2- Subtração')
+print('3- Multiplicação')
+print('4- Divisão')
+print('')
+escolha = input('Digite a opção desejada:')
+
+match escolha:
+    case "1": #ADIÇÃO
+        num1 = input("Digite o primeiro número: ")
+        num2 = input("Digite o segundo número: ")
+        resultado = num1 + num2
+        print('A sua soma é: ', resultado)
+        print('')
+
+    case "2": #SUBTRAÇÃO
+        num1 = input("Digite o primeiro número: ")
+        num2 = input("Digite o segundo número: ")
+        resultado = num1 - num2
+        print('A sua soma é: ', resultado)
+        print('')
+    
+    case "3": #MULTIPLICAÇÃO
+        num1 = input("Digite o primeiro número: ")
+        num2 = input("Digite o segundo número: ")
+        resultado = num1 * num2
+        print('A sua soma é: ', resultado)
+        print('')
+    
+    case "4": #DIVISÃO
+        num1 = input("Digite o primeiro número: ")
+        num2 = input("Digite o segundo número: ")
+        resultado = num1 / num2
+        print('A sua soma é: ', resultado)
+        print('')
